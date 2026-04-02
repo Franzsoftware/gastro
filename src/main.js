84d65c99b41c9af06e0964d4252ed09f00f00dd5
Francesc <113933140+Franzsoftware@users.noreply.github.com>
2026-04-02 15:44:07 +0200

    Header i footer

--- src/main.js
+++ src/main.js
@@ -2,3 +2,18 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 AOS.init();
+
+
+// Hover categorias 
+
+const cards = document.querySelectorAll('.cat');
+
+cards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        card.classList.add('hover');
+    })
+
+    card.addEventListener('mouseleave', () => {
+        card.classList.remove('hover');
+    })
+});
