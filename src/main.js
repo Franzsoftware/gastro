092a81156dcdda0bd1f9a8e45e330de1e22ec91a
Francesc <113933140+Franzsoftware@users.noreply.github.com>
2026-05-23 08:35:31 +0200

    rendimiento home, categorias, enlaces

--- src/main.js
+++ src/main.js
@@ -18,22 +18,30 @@ cards.forEach(card => {
     })
 });
 
-// Hamburger menú
-
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
-const icon = hamburger.querySelector('i');
+const iconPath = document.getElementById('menuPath');
 
 hamburger.addEventListener('click', () => {
+
     nav.classList.toggle('active');
 
     if(nav.classList.contains('active')) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-xmark');
+
+        iconPath.setAttribute(
+            'd',
+            'M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 178.7 150.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L178.7 224 105.4 297.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 269.3l73.4 73.4c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L269.3 224l73.3-73.4z'
+        );
+
     } else {
-        icon.classList.remove('fa-xmark');
-        icon.classList.add('fa-bars');
+
+        iconPath.setAttribute(
+            'd',
+            'M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z'
+        );
+
     }
+
 });
 
 
